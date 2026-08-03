bc15b7366db065f2026e5b310d2e471a907a5fad
Babotelli45 <linzarddd@gmail.com>
2026-08-03 04:45:01 +0300

    Update ReadingTracker.tsx

--- components/ReadingTracker.tsx
+++ components/ReadingTracker.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import ScribbleIcon from "./ScribbleIcon";
-import { BOOK_PATH } from "@/lib/iconShapes";
-
 interface Props {
   value: number; // 0-5
   onChange: (v: number) => void;
@@ -12,19 +9,20 @@ export default function ReadingTracker({ value, onChange }: Props) {
   return (
     <div className="flex items-center gap-1">
       {[1, 2, 3, 4, 5].map((i) => (
-        <ScribbleIcon
-       <button
-  key={i}
-  type="button"
-  onClick={() => onChange(i === value ? i - 1 : i)}
-  className="p-0.5 transition-opacity hover:opacity-80 focus:outline-none"
->
-  <img
-    src="/icons/okuma.png"
-    alt={`okuma ${i}`}
-    className={`w-6 h-6 object-contain ${i <= value ? 'opacity-100' : 'opacity-30 grayscale'}`}
-  />
-</button>
+        <button
+          key={i}
+          type="button"
+          onClick={() => onChange(i === value ? i - 1 : i)}
+          className="p-0.5 transition-opacity hover:opacity-80 focus:outline-none"
+        >
+          <img
+            src="/icons/okuma.png"
+            alt={`okuma ${i}`}
+            className={`w-6 h-6 object-contain ${
+              i <= value ? "opacity-100" : "opacity-30 grayscale"
+            }`}
+          />
+        </button>
       ))}
     </div>
   );
