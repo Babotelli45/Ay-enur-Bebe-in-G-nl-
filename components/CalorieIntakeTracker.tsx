290c4b272c8de71a22510fc9ac6527a459354586
Babotelli45 <linzarddd@gmail.com>
2026-08-04 00:49:06 +0300

    Update CalorieIntakeTracker.tsx

--- components/CalorieIntakeTracker.tsx
+++ components/CalorieIntakeTracker.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import TrackerRow from "./TrackerRow";
+
 interface Props {
   value: number; // 0-5
   onChange: (v: number) => void;
@@ -7,26 +9,12 @@ interface Props {
 
 export default function CalorieIntakeTracker({ value, onChange }: Props) {
   return (
-    <div className="flex items-center gap-1">
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button
-          key={i}
-          type="button"
-          onClick={() => onChange(i === value ? i - 1 : i)}
-          aria-label={`kalori alım yıldızı ${i}`}
-          aria-pressed={i <= value}
-          className="transition-transform hover:scale-110 active:scale-95"
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/yildiz.png"
-            alt=""
-            className={`w-6 h-6 object-contain mix-blend-multiply transition-opacity ${
-              i <= value ? "opacity-100" : "opacity-30"
-            }`}
-          />
-        </button>
-      ))}
-    </div>
+    <TrackerRow
+      value={value}
+      onChange={onChange}
+      iconSrc="/icons/yildiz.png"
+      fallbackEmoji="⭐"
+      labelPrefix="kalori alım yıldızı"
+    />
   );
 }
