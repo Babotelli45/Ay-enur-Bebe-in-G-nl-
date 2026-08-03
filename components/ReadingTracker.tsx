3a6ce264ebcbe582a94211b40580a701137eda20
Babotelli45 <linzarddd@gmail.com>
2026-08-04 00:48:12 +0300

    Update ReadingTracker.tsx

--- components/ReadingTracker.tsx
+++ components/ReadingTracker.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import TrackerRow from "./TrackerRow";
+
 interface Props {
   value: number; // 0-5
   onChange: (v: number) => void;
@@ -7,23 +9,12 @@ interface Props {
 
 export default function ReadingTracker({ value, onChange }: Props) {
   return (
-    <div className="flex items-center gap-1">
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button
-          key={i}
-          type="button"
-          onClick={() => onChange(i === value ? i - 1 : i)}
-          className="p-0.5 transition-opacity hover:opacity-80 focus:outline-none"
-        >
-          <img
-            src="/icons/okuma.png"
-            alt={`okuma ${i}`}
-            className={`w-6 h-6 object-contain ${
-              i <= value ? "opacity-100" : "opacity-30 grayscale"
-            }`}
-          />
-        </button>
-      ))}
-    </div>
+    <TrackerRow
+      value={value}
+      onChange={onChange}
+      iconSrc="/icons/okuma.png"
+      fallbackEmoji="📖"
+      labelPrefix="okuma"
+    />
   );
 }
